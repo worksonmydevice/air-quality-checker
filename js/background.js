55adb1c465d0a0042fca93daf50733264d59374f
WorksOnMyDevice <worksonmydevice.dev@gmail.com>
2017-10-01 22:03:28 +0200

    add popup notification for hardcoded station

--- js/background.js
+++ js/background.js
@@ -1,4 +1,7 @@
 var chmiPortalWebPageUrl = "http://portal.chmi.cz/files/portal/docs/uoco/web_generator/actual_hour_data_CZ.html"
+var chmiPortalJSONUrl = "http://portal.chmi.cz/files/portal/docs/uoco/web_generator/aqindex_cze.json"
+var regionCode = "T"
+var stationCode = "TFMIA"
 
 chrome.browserAction.onClicked.addListener(showChmiPortalWebPage);
 
@@ -12,4 +15,33 @@ function showChmiPortalWebPage() {
             chrome.tabs.create({url: chmiPortalWebPageUrl});
         }
     });
+    getAirQualityJSON();
+}
+
+var findRegionByCode = function (region) {
+    return region.Code === regionCode;
+}
+
+function findStationByCode(station) {
+    return station.Code === stationCode;
+}
+
+function getAirQualityJSON() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", chmiPortalJSONUrl, true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+            var resp = JSON.parse(xhr.responseText);
+            var state = resp.States[0]
+            var region = state.Regions.find(findRegionByCode)
+            var stationData = region.Stations.find(findStationByCode);
+            chrome.notifications.create("AQnotifID", {
+                                        title: "*** Air Quality Checker ***",
+                                        iconUrl: chrome.runtime.getURL('images/icon_128.png'),
+                                        type: "basic",
+                                        message: "Station: " + stationData.Name + "\nAQ status: " + stationData.Ix
+                                        }, function() {});
+        }
+    }
+    xhr.send();
 }
